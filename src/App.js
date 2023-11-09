a8fa708f040d36dbe531c1bc4d4d2340dd8b7c9b
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-09 10:17:42 +0300

    Added styles, created html

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import './App.scss';
 export default function App() {
   return (
     <>
+    <div id="forest"></div>
+    <div id="app-container">
+      <div id="app">
+        <div className="length-control">
+          <p id="break-label">Break Length</p>
+          <button id="break-increment"></button>
+          <p id="break-length">5</p>
+          <button id="break-decrement"></button>
+        </div>
+        <div className="length-control">
+          <p id="session-label">Session Length</p>
+            <button id="session-increment"></button>
+          <p id="session-length">25</p>
+            <button id="session-decrement"></button>
+        </div>
+        <div id="timer">
+          <p id="timer-label">Session x</p>
+          <p id="time-left">00:00</p>
+          <button id="start_stop">Start/Stop</button>
+          <button id="reset">Reset</button>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
