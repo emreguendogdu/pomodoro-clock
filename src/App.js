3a64d64d3a1b559b0de0b328c93bd255866630d9
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-09 14:03:46 +0300

    Improved timer functionality
    - Use states instead of fixed values
    
    - Add isSession logic, turns timer into break if false
    
    - Fix timer length constrains, now not working below 0 and above 60
    
    - Fix timer undefined error incrementing session length
    
    - Update initial values of the states
    
    - Improve start-stop logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import "./App.scss"
+import { useState, useRef } from 'react';
 
 export default function App() {
   // State variables for break length, session length, time left, and session number
   const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(25);
-  const [timeLeft, setTimeLeft] = useState("25:00");
-  const [sessionNum, setSessionNum] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
+  const [sessionLength, setSessionLength] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(`${sessionLength}:00`);
+  const [sessionNum, setSessionNum] = useState(1);
+  const [isSession, setIsSession] = useState(true);
+  const intervalIdRef = useRef(null);
 
   const handleValue = (e) => {
     // Get the event and action from the button's id
@@ -14,57 +16,79 @@ export default function App() {
     let event = id.split("-")[0];
     let action = id.split("-")[1];
 
+    // Quit if error occurs
+    if (action === undefined || event === undefined) return;
+
     // Get the current length and setter function based on the event
     const length = event === "break" ? breakLength : sessionLength;
     const setLength = event === "break" ? setBreakLength : setSessionLength;
 
-    // Update the length based on the action
-    if (length > 0 && length < 60) {
-      if (action === "decrement") {
-        setLength(prev => prev - 1);
-      } else if (action === "increment") {
+    // Update the length based on the action 
+    if (action === "increment") {
+      if (length !== 60) {
         setLength(prev => prev + 1);
       }
+    } else if (action === "decrement") {
+      if (length !== 0) {
+        setLength(prev => prev - 1);
+      }
     }
+
+    if (event === "session") {
+      setTimeLeft(`${sessionLength + (action === "increment" ? 1 : -1)}:00`);
+    };
+    if (event === "break" && isSession === false) {
+      setTimeLeft(`${breakLength + (action === "increment"? 1 : -1)}:00`);
+    };
   };
 
   const handleReset = () => {
-    // Reset the break length, session length, and time left
+    // Stop the timer
+    clearInterval(intervalIdRef.current);
+    intervalIdRef.current = null;
+    // Reset the break length, session length, time left, and session number
     setBreakLength(5);
     setSessionLength(25);
-    setTimeLeft("25:00");
+    setTimeLeft(`${sessionLength}:00`);
+    setSessionNum(1);
+    setIsSession(true);
   };
 
   const handleStartStop = () => {
-    // Increment the session number
-    setSessionNum(prev => prev + 1);
-
-    if (intervalId === null) {
+    if (intervalIdRef.current === null) {
       // Start the timer
+      let time = timeLeft.split(":");
+      let minutes = parseInt(time[0]);
+      let seconds = parseInt(time[1]);
       const id = setInterval(() => {
-        // Get the current minutes and seconds from the time left
-        let time = timeLeft.split(":");
-        let minutes = parseInt(time[0]);
-        let seconds = parseInt(time[1]);
-
         // Update the minutes and seconds
         if (seconds === 0) {
-          seconds = 59;
-          minutes = minutes - 1;
+          if (minutes === 0) {
+            // End of session/break, switch to the other one
+            setIsSession(prev => !prev);
+            setSessionNum(prev => prev + 1);
+            setTimeLeft(isSession ? `${breakLength}:00` : `${sessionLength}:00`);
+            clearInterval(intervalIdRef.current);
+            intervalIdRef.current = null;
+            return;
+          } else {
+            seconds = 59;
+            minutes = minutes - 1;
+          }
         } else {
-          seconds = seconds - 1;
+          seconds -= 1;
         }
 
         // Update the time left
-        setTimeLeft(`${minutes}:${seconds}`);
+        setTimeLeft(`${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`);
       }, 1000);
-
+      
       // Set the interval ID
-      setIntervalId(id);
+      intervalIdRef.current = id;
     } else {
       // Stop the timer
-      clearInterval(intervalId);
-      setIntervalId(null);
+      clearInterval(intervalIdRef.current);
+      intervalIdRef.current = null;
     }
   }
 
@@ -100,7 +124,7 @@ export default function App() {
 
           {/* Timer */}
           <div id="timer">
-            <p id="timer-label">Session {sessionNum}</p>
+            <p id="timer-label">{isSession ? 'Session' : 'Break'} {sessionNum}</p>
             <p id="time-left">{timeLeft}</p>
             <button id="start_stop" onClick={handleStartStop}>Start/Stop</button>
             <button id="reset" onClick={handleReset}>Reset</button>
