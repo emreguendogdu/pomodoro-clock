19755cd4515d87154d3e855c1008088009f02f12
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-09 11:10:58 +0300

    Added handleValue function for decrementing and incrementing both break - session lengths, changed forest img height, changed its border-bottom color to lightened bg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import './App.scss';
+import { useState } from 'react';
 
 export default function App() {
+  const [breakLength, setBreakLength] = useState("5");
+  const [sessionLength, setSessionLength] = useState("25");
+
+  const handleValue = (e) => {
+    let id = e.target.id;
+    let event = id.split("-")[0];
+    let action = id.split("-")[1];
+
+    const setLength = event === "break" ? setBreakLength : setSessionLength;
+      if (action === "decrement") {
+        setLength(prev => Number(prev) - 1);
+      } else if (action === "increment") {
+        setLength(prev => Number(prev) + 1);
+      };
+  };
+
+  const handleReset = () => {
+    setBreakLength("5");
+    setSessionLength("25");
+  };
+
   return (
     <>
     <div id="forest"></div>
@@ -8,21 +30,29 @@ export default function App() {
       <div id="app">
         <div className="length-control">
           <p id="break-label">Break Length</p>
-          <button id="break-increment"></button>
-          <p id="break-length">5</p>
-          <button id="break-decrement"></button>
+          <button id="break-increment" onClick={handleValue}>
+            <i className="fa fa-arrow-up"></i>
+          </button>
+          <p id="break-length">{breakLength}</p>
+          <button id="break-decrement" onClick={handleValue}>
+            <i className="fa fa-arrow-down"></i>
+          </button>
         </div>
         <div className="length-control">
           <p id="session-label">Session Length</p>
-            <button id="session-increment"></button>
-          <p id="session-length">25</p>
-            <button id="session-decrement"></button>
+            <button id="session-increment" onClick={handleValue}>
+            <i className="fa fa-arrow-up"></i>
+            </button>
+          <p id="session-length">{sessionLength}</p>
+            <button id="session-decrement" onClick={handleValue}>
+            <i className="fa fa-arrow-down"></i>
+            </button>
         </div>
         <div id="timer">
           <p id="timer-label">Session x</p>
           <p id="time-left">00:00</p>
           <button id="start_stop">Start/Stop</button>
-          <button id="reset">Reset</button>
+          <button id="reset" onClick={handleReset}>Reset</button>
         </div>
       </div>
     </div>
