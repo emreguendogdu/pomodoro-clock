2b6ed509186f135ae5ca71360a09741e1f2f2ed3
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-12-22 22:14:05 +0300

    feat: change space bg

--- src/components/visibles/Timer.js
+++ src/components/visibles/Timer.js
@@ -1,156 +1,156 @@
-import React, { useState, useRef, useEffect } from "react";
-import { LengthControl } from "./LengthControl";
-import { TimerDisplay } from "./TimerDisplay";
-import { handleAudio } from "../AudioPlayer";
-import TimerPresets from "./TimerPresets";
+import React, { useState, useRef, useEffect } from "react"
+import { LengthControl } from "./LengthControl"
+import { TimerDisplay } from "./TimerDisplay"
+import { handleAudio } from "../AudioPlayer"
+import TimerPresets from "./TimerPresets"
 
 export function Timer() {
-  const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(25);
-  const [labelDisplay, setLabelDisplay] = useState("Session 1");
-  const intervalIdRef = useRef(null);
+  const [breakLength, setBreakLength] = useState(5)
+  const [sessionLength, setSessionLength] = useState(25)
+  const [labelDisplay, setLabelDisplay] = useState("Session 1")
+  const intervalIdRef = useRef(null)
 
   // Format time in MM:SS
   const formatTime = (minutes, seconds) => {
-    const formattedMinutes = String(minutes).padStart(2, "0");
-    const formattedSeconds = String(seconds).padStart(2, "0");
-    return `${formattedMinutes}:${formattedSeconds}`;
-  };
+    const formattedMinutes = String(minutes).padStart(2, "0")
+    const formattedSeconds = String(seconds).padStart(2, "0")
+    return `${formattedMinutes}:${formattedSeconds}`
+  }
 
   // Update the display with the given length
   const updateDisplay = (length) => {
-    const minutes = Math.floor(length / 60);
-    const seconds = length % 60;
+    const minutes = Math.floor(length / 60)
+    const seconds = length % 60
     document.getElementById("time-left").innerText = formatTime(
       minutes,
       seconds
-    );
-  };
+    )
+  }
 
   const sessionIncrement = () => {
-    setSessionLength((prev) => (prev < 60 ? prev + 1 : prev));
-  };
+    setSessionLength((prev) => (prev < 60 ? prev + 1 : prev))
+  }
   const sessionDecrement = () => {
-    setSessionLength((prev) => (prev > 1 ? prev - 1 : prev));
-  };
+    setSessionLength((prev) => (prev > 1 ? prev - 1 : prev))
+  }
 
   const breakIncrement = () => {
-    setBreakLength((prev) => (prev < 60 ? prev + 1 : prev));
-  };
+    setBreakLength((prev) => (prev < 60 ? prev + 1 : prev))
+  }
   const breakDecrement = () => {
-    setBreakLength((prev) => (prev > 1 ? prev - 1 : prev));
-  };
+    setBreakLength((prev) => (prev > 1 ? prev - 1 : prev))
+  }
 
   // Reset all settings to default
   const resetTimer = () => {
-    const defaultSessionLength = 25;
-    stopInterval();
-    handleAudio("stop");
-    setBreakLength(5);
-    setSessionLength(defaultSessionLength);
-    updateDisplay(defaultSessionLength * 60);
-    timerDuration = defaultSessionLength * 60;
-    sessionNum = 1;
-    setLabelDisplay("Session 1");
-    handleTimerLabel("reset");
-  };
+    const defaultSessionLength = 25
+    stopInterval()
+    handleAudio("stop")
+    setBreakLength(5)
+    setSessionLength(defaultSessionLength)
+    updateDisplay(defaultSessionLength * 60)
+    timerDuration = defaultSessionLength * 60
+    sessionNum = 1
+    setLabelDisplay("Session 1")
+    handleTimerLabel("reset")
+  }
 
   // Handle timer label display based on the given value
   const handleTimerLabel = (value) => {
-    const timerLabel = document.getElementById("timer-label");
-    const timerLabelP = document.querySelector("#timer-label p");
+    const timerLabel = document.getElementById("timer-label")
+    const timerLabelP = document.querySelector("#timer-label p")
 
     if (value === "break") {
       // Display "Break" label
-      setLabelDisplay("Break");
-      timerLabelP.classList.remove("border-bottom");
+      setLabelDisplay("Break")
+      timerLabelP.classList.remove("border-bottom")
 
       // Display the next session information
       if (!document.querySelector(".break-span")) {
-        const span = document.createElement("span");
-        span.classList.add("break-span");
-        span.innerText = `Next session: ${sessionNum}`;
-        timerLabel.appendChild(span);
+        const span = document.createElement("span")
+        span.classList.add("break-span")
+        span.innerText = `Next session: ${sessionNum}`
+        timerLabel.appendChild(span)
       }
     } else if (value === "session") {
       // Remove the next session information when returning to a session
       if (document.querySelector(".break-span")) {
-        timerLabel.removeChild(document.querySelector(".break-span"));
+        timerLabel.removeChild(document.querySelector(".break-span"))
       }
-      setLabelDisplay(`Session ${sessionNum}`);
-      timerLabelP.classList.add("border-bottom");
+      setLabelDisplay(`Session ${sessionNum}`)
+      timerLabelP.classList.add("border-bottom")
     } else if (value === "reset") {
       // Remove the next session information when returning to a session
       if (document.querySelector(".break-span")) {
-        timerLabel.removeChild(document.querySelector(".break-span"));
+        timerLabel.removeChild(document.querySelector(".break-span"))
       }
-      timerLabelP.classList.add("border-bottom");
+      timerLabelP.classList.add("border-bottom")
     }
-  };
+  }
 
   // Stop the running interval
   const stopInterval = () => {
-    clearInterval(intervalIdRef.current);
-    intervalIdRef.current = null;
-  };
+    clearInterval(intervalIdRef.current)
+    intervalIdRef.current = null
+  }
 
   // Initializations
-  let timerDuration = 60 * sessionLength;
-  let sessionNum = 1;
-  let timerStatus;
+  let timerDuration = 60 * sessionLength
+  let sessionNum = 1
+  let timerStatus
 
   // Handle start and stop of the timer
   const handleStartStop = () => {
     if (intervalIdRef.current === null) {
-      handleAudio("stop");
+      handleAudio("stop")
 
       // Start the interval
       const id = setInterval(() => {
-        const timerDisplay = document.getElementById("time-left");
-        timerDisplay.classList.add("highlight");
-        setTimeout(() => timerDisplay.classList.remove("highlight"), 1000);
+        const timerDisplay = document.getElementById("time-left")
+        timerDisplay.classList.add("highlight")
+        setTimeout(() => timerDisplay.classList.remove("highlight"), 1000)
         if (timerDuration === 0) {
           // When timer reaches 0
-          handleAudio("play");
+          handleAudio("play")
 
           if (timerStatus !== "break") {
             // Switch to break time
-            handleTimerLabel("break");
-            timerDuration = breakLength * 60;
-            sessionNum += 1;
-            timerStatus = "break";
+            handleTimerLabel("break")
+            timerDuration = breakLength * 60
+            sessionNum += 1
+            timerStatus = "break"
           } else {
             // Switch back to session time
-            handleTimerLabel("session");
-            timerDuration = sessionLength * 60;
-            timerStatus = "session";
+            handleTimerLabel("session")
+            timerDuration = sessionLength * 60
+            timerStatus = "session"
           }
         } else {
           // Decrease the timer duration
-          timerDuration -= 1;
+          timerDuration -= 1
         }
-        updateDisplay(timerDuration);
-      }, 1000);
+        updateDisplay(timerDuration)
+      }, 1000)
 
       // Set timer to running
-      intervalIdRef.current = id;
+      intervalIdRef.current = id
     } else {
       // Stop the running interval if the timer is already running
-      stopInterval();
+      stopInterval()
     }
-  };
+  }
 
   // Update the display when the session length changes
   useEffect(() => {
-    updateDisplay(sessionLength * 60);
-  });
+    updateDisplay(sessionLength * 60)
+  })
 
   // Cleanup on component unmount
   useEffect(() => {
     return () => {
-      clearInterval(intervalIdRef.current);
-    };
-  }, []);
+      clearInterval(intervalIdRef.current)
+    }
+  }, [])
   return (
     <>
       <div id="app">
@@ -176,5 +176,5 @@ export function Timer() {
         <TimerPresets setSession={setSessionLength} setBreak={setBreakLength} />
       </div>
     </>
-  );
+  )
 }
