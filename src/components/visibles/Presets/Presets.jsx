0c2dfe50bfc8bfe0ebada348a24714f995c60091
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-01-05 16:06:59 +0300

    refactor: simplify repeating setSession-break code

--- src/components/visibles/Presets/Presets.jsx
+++ src/components/visibles/Presets/Presets.jsx
@@ -1,13 +1,16 @@
 import "./Presets.css"
 export default function TimerPresets(props) {
+  const setSession = props.setSession
+  const setBreak = props.setBreak
+
   const TimerPreset = (props) => {
     return (
       <>
         <button
           id={`${props.s}-${props.b}-preset`}
           onClick={() => {
-            props.setS(+props.s)
-            props.setB(+props.b)
+            setSession(+props.s)
+            setBreak(+props.b)
           }}
         >
           {props.s} - {props.b}
@@ -18,30 +21,10 @@ export default function TimerPresets(props) {
   return (
     <section id="presets-section">
       <div id="presets-container">
-        <TimerPreset
-          s="25"
-          b="5"
-          setS={props.setSession}
-          setB={props.setBreak}
-        />
-        <TimerPreset
-          s="50"
-          b="10"
-          setS={props.setSession}
-          setB={props.setBreak}
-        />
-        <TimerPreset
-          s="60"
-          b="10"
-          setS={props.setSession}
-          setB={props.setBreak}
-        />
-        <TimerPreset
-          s="90"
-          b="10"
-          setS={props.setSession}
-          setB={props.setBreak}
-        />
+        <TimerPreset s="25" b="5" />
+        <TimerPreset s="50" b="10" />
+        <TimerPreset s="60" b="10" />
+        <TimerPreset s="90" b="10" />
       </div>
     </section>
   )
